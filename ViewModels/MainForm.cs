459f9e579a2bb94d5f8306b82e306a1911cc876f
Maksim Kotelnikov <m.kotelnikov@tbank.ru>
2025-11-27 05:22:15 +0300

    fix

--- ViewModels/MainForm.cs
+++ ViewModels/MainForm.cs
@@ -1,247 +1,12 @@
 using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using CSLab3.Models;
 using CSLab3.Interfaces;
 
 namespace CSLab3.ViewModels
 {
-    public class MainViewModel : ViewModelBase
-    {
-        private ObservableCollection<BlastFurnace> _furnaces;
-        private ObservableCollection<Worker> _workers;
-        private ObservableCollection<MaterialLoader> _loaders;
-        private string _logText;
-        
-        public ObservableCollection<BlastFurnace> Furnaces
-        {
-            get => _furnaces;
-            set
-            {
-                _furnaces = value;
-                OnPropertyChanged(nameof(Furnaces));
-            }
-        }
-        
-        public ObservableCollection<Worker> Workers
-        {
-            get => _workers;
-            set
-            {
-                _workers = value;
-                OnPropertyChanged(nameof(Workers));
-            }
-        }
-        
-        public ObservableCollection<MaterialLoader> Loaders
-        {
-            get => _loaders;
-            set
-            {
-                _loaders = value;
-                OnPropertyChanged(nameof(Loaders));
-            }
-        }
-        
-        public string LogText
-        {
-            get => _logText;
-            set
-            {
-                _logText = value;
-                OnPropertyChanged(nameof(LogText));
-            }
-        }
-        
-        public MainViewModel()
-        {
-            _furnaces = new ObservableCollection<BlastFurnace>();
-            _workers = new ObservableCollection<Worker>();
-            _loaders = new ObservableCollection<MaterialLoader>();
-            _logText = "";
-            
-            CreateInitialModels();
-        }
-        
-        private void CreateInitialModels()
-        {
-            var furnace = new BlastFurnace("Печь #1");
-            furnace.MaterialDepleted += Furnace_MaterialDepleted;
-            furnace.Overheat += Furnace_Overheat;
-            furnace.StatusChanged += Furnace_StatusChanged;
-            furnace.TemperatureChanged += Furnace_TemperatureChanged;
-            _furnaces.Add(furnace);
-            
-            var worker = new Worker("Иван Иванов", 5);
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.StatusChanged += Worker_StatusChanged;
-            _workers.Add(worker);
-            
-            var loader = new MaterialLoader("Загрузчик #1");
-            loader.MaterialLoaded += Loader_MaterialLoaded;
-            loader.LoadingStatusChanged += Loader_LoadingStatusChanged;
-            _loaders.Add(loader);
-            
-            furnace.Start();
-            worker.StartWork(loader);
-        }
-        
-        private void Furnace_MaterialDepleted(object sender, EventArgs e)
-        {
-            var furnace = sender as BlastFurnace;
-            LogMessage($"Внимание: {furnace?.Name} исчерпал материалы!");
-            
-            foreach (var worker in _workers)
-            {
-                worker.RespondToEvent("MaterialDepleted");
-            }
-        }
-        
-        private void Furnace_Overheat(object sender, EventArgs e)
-        {
-            var furnace = sender as BlastFurnace;
-            LogMessage($"Внимание: {furnace?.Name} перегревается!");
-            
-            foreach (var worker in _workers)
-            {
-                worker.RespondToEvent("Overheat");
-            }
-        }
-        
-        private void Furnace_StatusChanged(object sender, string e)
-        {
-            var furnace = sender as BlastFurnace;
-            LogMessage($"[{furnace?.Name}] {e}");
-        }
-        
-        private void Furnace_TemperatureChanged(object sender, int e)
-        {
-        }
-        
-        private void Worker_WorkPerformed(object sender, string e)
-        {
-            var worker = sender as Worker;
-            LogMessage($"[{worker?.Name}] Выполняет: {e}");
-        }
-        
-        private void Worker_StatusChanged(object sender, string e)
-        {
-            var worker = sender as Worker;
-            LogMessage($"[{worker?.Name}] {e}");
-        }
-        
-        private void Loader_MaterialLoaded(object sender, EventArgs e)
-        {
-            var loader = sender as MaterialLoader;
-            LogMessage($"[{loader?.Name}] Материалы успешно загружены");
-            
-            // Load specific amounts to the most appropriate furnace
-            if (_furnaces.Any())
-            {
-                // Find the furnace that needs this material the most
-                var targetFurnace = SelectAppropriateFurnace(loader);
-                
-                if (targetFurnace != null)
-                {
-                    // Load specific amounts based on what the loader was set to load
-                    if (loader != null)
-                    {
-                        switch (loader.CurrentMaterial)
-                        {
-                            case "Железная руда":
-                                targetFurnace.AddMaterials(loader.Quantity, 0, 0);
-                                break;
-                            case "Кокс":
-                                targetFurnace.AddMaterials(0, loader.Quantity, 0);
-                                break;
-                            case "Известняк":
-                                targetFurnace.AddMaterials(0, 0, loader.Quantity);
-                                break;
-                            default:
-                                // Load balanced amounts if material type is unknown
-                                targetFurnace.AddMaterials(
-                                    loader.Quantity / 3,
-                                    loader.Quantity / 3,
-                                    loader.Quantity / 3);
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        // Fallback to random amounts if loader is null
-                        targetFurnace.AddMaterials(
-                            20,
-                            20,
-                            10);
-                    }
-                }
-            }
-        }
-        
-        private BlastFurnace SelectAppropriateFurnace(MaterialLoader loader)
-        {
-            if (loader == null || !_furnaces.Any())
-                return null;
-            
-            // Find furnace with lowest level of the required material
-            BlastFurnace targetFurnace = null;
-            
-            switch (loader.CurrentMaterial)
-            {
-                case "Железная руда":
-                    targetFurnace = _furnaces.OrderBy(f => f.IronOre).FirstOrDefault();
-                    break;
-                case "Кокс":
-                    targetFurnace = _furnaces.OrderBy(f => f.Coke).FirstOrDefault();
-                    break;
-                case "Известняк":
-                    targetFurnace = _furnaces.OrderBy(f => f.Limestone).FirstOrDefault();
-                    break;
-                default:
-                    // If material type is unknown, use the first furnace
-                    targetFurnace = _furnaces.FirstOrDefault();
-                    break;
-            }
-            
-            return targetFurnace;
-        }
-        
-        private void Loader_LoadingStatusChanged(object sender, string e)
-        {
-            var loader = sender as MaterialLoader;
-            LogMessage($"[{loader?.Name}] {e}");
-        }
-        
-        public void LogMessage(string message)
-        {
-            LogText += $"\r\n[{DateTime.Now:HH:mm:ss}] {message}";
-            OnPropertyChanged(nameof(LogText));
-        }
-        
-        public void Cleanup()
-        {
-            foreach (var furnace in _furnaces)
-            {
-                furnace.Stop();
-            }
-            
-            foreach (var worker in _workers)
-            {
-                worker.StopWork();
-            }
-            
-            foreach (var loader in _loaders)
-            {
-                loader.StopLoading();
-            }
-        }
-    }
-    
     public partial class MainForm : Form
     {
         private MainViewModel _viewModel;
@@ -250,7 +15,7 @@ namespace CSLab3.ViewModels
         private Button _addWorkerButton;
         private Button _addLoaderButton;
         private Button _startStopButton;
-        private bool _isSimulationRunning = true;
+        private bool _isSimulationRunning = false;
         private System.Windows.Forms.Timer _animationTimer;
         private System.Windows.Forms.Timer _logUpdateTimer;
         private Panel _animationPanel;
@@ -258,128 +23,42 @@ namespace CSLab3.ViewModels
         // Event handlers for furnace
         private void Furnace_MaterialDepleted(object sender, EventArgs e)
         {
-            var furnace = sender as BlastFurnace;
-            _viewModel.LogMessage($"Внимание: {furnace?.Name} исчерпал материалы!");
-            
-            foreach (var worker in _viewModel.Workers)
-            {
-                worker.RespondToEvent("MaterialDepleted");
-            }
+            _viewModel.Furnace_MaterialDepleted(sender, e);
         }
         
         private void Furnace_Overheat(object sender, EventArgs e)
         {
-            var furnace = sender as BlastFurnace;
-            _viewModel.LogMessage($"Внимание: {furnace?.Name} перегревается!");
-            
-            foreach (var worker in _viewModel.Workers)
-            {
-                worker.RespondToEvent("Overheat");
-            }
+            _viewModel.Furnace_Overheat(sender, e);
         }
         
         private void Furnace_StatusChanged(object sender, string e)
         {
-            var furnace = sender as BlastFurnace;
-            _viewModel.LogMessage($"[{furnace?.Name}] {e}");
+            _viewModel.Furnace_StatusChanged(sender, e);
         }
         
         private void Furnace_TemperatureChanged(object sender, int e)
         {
+            _viewModel.Furnace_TemperatureChanged(sender, e);
         }
         
         private void Worker_WorkPerformed(object sender, string e)
         {
-            var worker = sender as Worker;
-            _viewModel.LogMessage($"[{worker?.Name}] Выполняет: {e}");
+            _viewModel.Worker_WorkPerformed(sender, e);
         }
         
         private void Worker_StatusChanged(object sender, string e)
         {
-            var worker = sender as Worker;
-            _viewModel.LogMessage($"[{worker?.Name}] {e}");
+            _viewModel.Worker_StatusChanged(sender, e);
         }
         
         private void Loader_MaterialLoaded(object sender, EventArgs e)
         {
-            var loader = sender as MaterialLoader;
-            _viewModel.LogMessage($"[{loader?.Name}] Материалы успешно загружены");
-            
-            // Load specific amounts to the most appropriate furnace
-            if (_viewModel.Furnaces.Any())
-            {
-                // Find the furnace that needs this material the most
-                var targetFurnace = SelectAppropriateFurnace(loader);
-                
-                if (targetFurnace != null)
-                {
-                    // Load specific amounts based on what the loader was set to load
-                    if (loader != null)
-                    {
-                        switch (loader.CurrentMaterial)
-                        {
-                            case "Железная руда":
-                                targetFurnace.AddMaterials(loader.Quantity, 0, 0);
-                                break;
-                            case "Кокс":
-                                targetFurnace.AddMaterials(0, loader.Quantity, 0);
-                                break;
-                            case "Известняк":
-                                targetFurnace.AddMaterials(0, 0, loader.Quantity);
-                                break;
-                            default:
-                                // Load balanced amounts if material type is unknown
-                                targetFurnace.AddMaterials(
-                                    loader.Quantity / 3,
-                                    loader.Quantity / 3,
-                                    loader.Quantity / 3);
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        // Fallback to random amounts if loader is null
-                        targetFurnace.AddMaterials(
-                            20,
-                            20,
-                            10);
-                    }
-                }
-            }
-        }
-        
-        private BlastFurnace SelectAppropriateFurnace(MaterialLoader loader)
-        {
-            if (loader == null || !_viewModel.Furnaces.Any())
-                return null;
-            
-            // Find furnace with lowest level of the required material
-            BlastFurnace targetFurnace = null;
-            
-            switch (loader.CurrentMaterial)
-            {
-                case "Железная руда":
-                    targetFurnace = _viewModel.Furnaces.OrderBy(f => f.IronOre).FirstOrDefault();
-                    break;
-                case "Кокс":
-                    targetFurnace = _viewModel.Furnaces.OrderBy(f => f.Coke).FirstOrDefault();
-                    break;
-                case "Известняк":
-                    targetFurnace = _viewModel.Furnaces.OrderBy(f => f.Limestone).FirstOrDefault();
-                    break;
-                default:
-                    // If material type is unknown, use the first furnace
-                    targetFurnace = _viewModel.Furnaces.FirstOrDefault();
-                    break;
-            }
-            
-            return targetFurnace;
+            _viewModel.Loader_MaterialLoaded(sender, e);
         }
         
         private void Loader_LoadingStatusChanged(object sender, string e)
         {
-            var loader = sender as MaterialLoader;
-            _viewModel.LogMessage($"[{loader?.Name}] {e}");
+            _viewModel.Loader_LoadingStatusChanged(sender, e);
         }
         
         public MainForm()
@@ -407,7 +86,7 @@ namespace CSLab3.ViewModels
         {
             _animationPanel = new Panel();
             _animationPanel.Location = new System.Drawing.Point(10, 10);
-            _animationPanel.Size = new System.Drawing.Size(600, 400);
+            _animationPanel.Size = new System.Drawing.Size(600, 450);
             _animationPanel.BorderStyle = BorderStyle.FixedSingle;
             _animationPanel.Paint += AnimationPanel_Paint;
             this.Controls.Add(_animationPanel);
@@ -418,7 +97,7 @@ namespace CSLab3.ViewModels
             _logUpdateTimer.Start();
             
             _logTextBox = new TextBox();
-            _logTextBox.Location = new System.Drawing.Point(10, 420);
+            _logTextBox.Location = new System.Drawing.Point(10, 470);
             _logTextBox.Size = new System.Drawing.Size(980, 300);
             _logTextBox.Multiline = true;
             _logTextBox.ScrollBars = ScrollBars.Vertical;
@@ -449,14 +128,14 @@ namespace CSLab3.ViewModels
             _startStopButton = new Button();
             _startStopButton.Location = new System.Drawing.Point(620, 130);
             _startStopButton.Size = new System.Drawing.Size(150, 30);
-            _startStopButton.Text = "Остановить симуляцию";
+            _startStopButton.Text = "Запустить симуляцию";
             _startStopButton.Click += StartStopButton_Click;
             this.Controls.Add(_startStopButton);
             
             _animationTimer = new System.Windows.Forms.Timer();
             _animationTimer.Interval = 50;
             _animationTimer.Tick += AnimationTimer_Tick;
-            _animationTimer.Start();
+            _animationTimer.Stop(); // Start with animation stopped
             
             BindToViewModel();
         }
@@ -469,8 +148,13 @@ namespace CSLab3.ViewModels
         private void UpdateLogDisplay()
         {
             _logTextBox.Text = _viewModel.LogText;
-            _logTextBox.SelectionStart = _logTextBox.Text.Length;
-            _logTextBox.ScrollToCaret();
+            
+            // Only auto-scroll to bottom when simulation is running
+            if (_isSimulationRunning)
+            {
+                _logTextBox.SelectionStart = _logTextBox.Text.Length;
+                _logTextBox.ScrollToCaret();
+            }
         }
         
         private void AddFurnaceButton_Click(object sender, EventArgs e)
@@ -582,7 +266,7 @@ namespace CSLab3.ViewModels
             UpdateLogDisplay();
         }
         
-        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if (e.PropertyName == nameof(MainViewModel.LogText))
             {
